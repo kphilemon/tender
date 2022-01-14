09874f249bda9a2b6209759e2af6b364506486cf
PrasMaran <prasmaran9999@gmail.com>
2022-01-14 19:10:16 +0800

    app skeleton

---
+++ app/src/main/java/com/example/tender/application/MyApplication.java
@@ -0,0 +1,8 @@
+package com.example.tender.application;
+
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+}
---
+++ app/src/main/java/com/example/tender/utils/appUtils/AppUtils.java
@@ -0,0 +1,13 @@
+package com.example.tender.utils.appUtils;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class AppUtils {
+
+    public static void toast(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+}
