c1f516938722f06f6ac6c38ba7827e77c35ab6ea
PrasMaran <prasmaran9999@gmail.com>
2022-01-15 05:09:24 +0800

    Added basic view pager to main activity

--- app/src/main/java/com/example/tender/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/tender/ui/activity/MainActivity.java
@@ -2,36 +2,77 @@ package com.example.tender.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
+
 import com.example.tender.R;
+import com.example.tender.utils.adapter.ViewPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button startBtn;
+    private ViewPager2 viewPager;
+    private BottomNavigationView bottomNavigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startBtn = findViewById(R.id.main_go_btn);
+        viewPager = findViewById(R.id.main_view_pager);
+        bottomNavigationView = findViewById(R.id.main_bottom_nav);
+        bottomNavigationView.setItemIconTintList(null);
 
-        startBtn.setOnClickListener(new View.OnClickListener() {
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.matches));
+
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
-            public void onClick(View v) {
-                goToUserPref();
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position) {
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.main_matches_nav).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.main_friends_nav).setChecked(true);
+                        break;
+                }
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.main_matches_nav:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.main_friends_nav:
+                        viewPager.setCurrentItem(1);
+                        break;
+                }
+
+                return true;
             }
         });
 
     }
 
     public void goToUserPref() {
-        Intent intent = new Intent(this, UserPreferenceActivity.class);
-        intent.putExtra("tender", "Tinder for Food");
-        startActivity(intent);
+
     }
 
 }
