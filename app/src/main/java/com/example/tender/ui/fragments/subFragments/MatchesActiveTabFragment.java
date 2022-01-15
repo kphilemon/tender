22bc7c8bdddfd6847568bee754ae898793beb6e8
PrasMaran <prasmaran9999@gmail.com>
2022-01-16 04:41:17 +0800

    Added view pagers for Matches and Friends

---
+++ app/src/main/java/com/example/tender/ui/fragments/subFragments/MatchesActiveTabFragment.java
@@ -0,0 +1,39 @@
+package com.example.tender.ui.fragments.subFragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tender.R;
+
+
+public class MatchesActiveTabFragment extends Fragment {
+
+
+    public MatchesActiveTabFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_matches_active_tab, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
