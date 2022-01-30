aa168a0059055a8d8aec0a3e7775ea3d153aaa73
sameer <sameergori768@gmail.com>
2022-01-31 02:30:59 +0530

    Sammer whole part

--- app/src/main/java/com/example/tender/application/SignUpActivity.java
+++ app/src/main/java/com/example/tender/application/SignUpActivity.java
@@ -1,10 +1,17 @@
 package com.example.tender.application;
 
 
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.TextWatcher;
+import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +20,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import com.example.tender.R;
 import com.google.android.material.textfield.TextInputEditText;
@@ -38,12 +46,7 @@ public class SignUpActivity extends AppCompatActivity {
         textInputLayout = (TextInputLayout) findViewById(R.id.textinputlayout);
         user = (TextInputEditText) findViewById(R.id.username);
 
-        confirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                validate();
-            }
-        });
+
 
         TextView view = findViewById(R.id.textview2);
         String text = "Pick wisely because once you get a name, you\n" +
@@ -65,41 +68,69 @@ public class SignUpActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
+        user.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                String val = textInputLayout.getEditText().getText().toString().trim();
+
+
+                if (val.isEmpty()) {
+                    textInputLayout.setError("Field cannot be empty");
+                    textInputLayout.setErrorIconDrawable(getResources().getDrawable(R.drawable.wrong));
+                } else if (val.length() < 7) {
+                    textInputLayout.setError("Minimum 7 characters required");
+                    textInputLayout.setErrorIconDrawable(getResources().getDrawable(R.drawable.wrong));
+                } else if (isStringOnlyAlphabet(val) == false) {
+                    textInputLayout.setError("Invalid Username");
+                    textInputLayout.setErrorIconDrawable(getResources().getDrawable(R.drawable.wrong));
+                } else if (val.length() > 25) {
+                    textInputLayout.setError("Username should be under 25 characters");
+                    textInputLayout.setErrorIconDrawable(getResources().getDrawable(R.drawable.wrong));
+                } else if(val.length()>=7 && val.length()<=25 && isStringOnlyAlphabet(val) == true ){
+
+                    int errorColor;
+                    final int version = Build.VERSION.SDK_INT;
+
+                    //Get the defined errorColor from color resource.
+                    if (version >= 23) {
+                        errorColor = ContextCompat.getColor(getApplicationContext(), R.color.green);
+                    } else {
+                        errorColor = getResources().getColor(R.color.green);
+                    }
+
+
+                    String errorString = "Great name! It's not taken, so it's all yours";
+                    ForegroundColorSpan foregroundColorSpan = new ForegroundColorSpan(errorColor);
+                    SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(errorString);
+                    spannableStringBuilder.setSpan(foregroundColorSpan, 0, errorString.length(), 0);
+                    textInputLayout.setError(spannableStringBuilder);
+                    textInputLayout.setBoxStrokeColor(getResources().getColor(R.color.gray));
+                    textInputLayout.setErrorIconDrawable(getResources().getDrawable(R.drawable.correct));
+                }
+                else {
+                    textInputLayout.setError(null);
+                    textInputLayout.setErrorEnabled(false);
+                }
+            }
+        });
+
 
     }
 
-    public void validate(){
-
-        if(!validusername()){
-            return;
-        }
-    }
-
-
-    private boolean validusername() {
-
-        String val = textInputLayout.getEditText().getText().toString().trim();
-
-
-        if (val.isEmpty()) {
-            textInputLayout.setError("Field cannot be empty");
-            return false;
-        } else if (val.length() < 7) {
-            textInputLayout.setError("Minimum 7 characters required");
-            return false;
-        } else if (isStringOnlyAlphabet(val) == false) {
-            textInputLayout.setError("Invalid Username");
-            return false;
-        } else if (val.length() > 25) {
-            textInputLayout.setError("Username is too large");
-            return false;
-        } else {
-            textInputLayout.setError(null);
-            textInputLayout.setErrorEnabled(false);
-            return true;
-        }
-
-    }
 
 
     private static boolean isStringOnlyAlphabet(String str) {
