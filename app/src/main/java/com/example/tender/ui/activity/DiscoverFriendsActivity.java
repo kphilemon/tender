0f9cef4943b6a3f7a199b6554fcded57d6dfa825
PrasMaran <prasmaran9999@gmail.com>
2022-01-31 04:44:02 +0800

    Added discover friends recycler view + search filter

--- app/src/main/java/com/example/tender/ui/activity/DiscoverFriendsActivity.java
+++ app/src/main/java/com/example/tender/ui/activity/DiscoverFriendsActivity.java
@@ -1,17 +1,30 @@
 package com.example.tender.ui.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tender.R;
+import com.example.tender.model.AddFriend;
+import com.example.tender.ui.RecyclerViewInterface;
+import com.example.tender.utils.adapter.recyclerAdapter.DiscoverNewFriendsAdapter;
+import com.example.tender.utils.appUtils.AppUtils;
 import com.google.android.material.appbar.AppBarLayout;
+import com.todkars.shimmer.ShimmerRecyclerView;
 
-public class DiscoverFriendsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class DiscoverFriendsActivity extends AppCompatActivity implements RecyclerViewInterface {
+
+    private ShimmerRecyclerView recyclerView;
+    private SearchView searchView;
+    DiscoverNewFriendsAdapter discoverNewFriendsAdapter;
+    ArrayList<AddFriend> discoverFriendList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +32,18 @@ public class DiscoverFriendsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_discover_friends);
 
         setupToolBar();
+        discoverFriendList = new ArrayList<AddFriend>();
+
+        recyclerView = findViewById(R.id.discover_friends_recyclerView);
+        setupDiscoverFriendsList();
+        //setAdapter();
+
     }
 
     private void setupToolBar() {
 
         AppBarLayout toolbar = findViewById(R.id.discover_friends_toolbar);
-        SearchView searchView = toolbar.findViewById(R.id.userNameSearchView);
+        searchView = toolbar.findViewById(R.id.userNameSearchView);
         searchView.setVisibility(View.VISIBLE);
 
         ImageView backButtonImage = toolbar.findViewById(R.id.back_arrow_icon);
@@ -33,12 +52,73 @@ public class DiscoverFriendsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
-//                Intent intent = new Intent(DiscoverFriendsActivity.this, MainActivity.class);
-//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                startActivity(intent);
             }
         });
 
+        /**
+         * Perform search filter
+         * but something weird happening to the
+         * list item of add friend list items
+         */
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                discoverNewFriendsAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
 
+    // Dummy data
+    private void setupDiscoverFriendsList() {
+        discoverFriendList.add(new AddFriend("Spider-man",true));
+        discoverFriendList.add(new AddFriend("Spider-boy",false));
+        discoverFriendList.add(new AddFriend("Iron-man",false));
+        discoverFriendList.add(new AddFriend("Panther-man",false));
+        discoverFriendList.add(new AddFriend("Awesome-man",false));
+        discoverFriendList.add(new AddFriend("Unknown-man",false));
+        discoverFriendList.add(new AddFriend("123-man",true));
+        discoverFriendList.add(new AddFriend("456-man",false));
+        discoverFriendList.add(new AddFriend("789-man",false));
+        discoverFriendList.add(new AddFriend("Chick-man",true));
+        discoverFriendList.add(new AddFriend("Fillet-man",true));
+
+        setAdapter();
+
+    }
+
+    public void setAdapter() {
+        discoverNewFriendsAdapter = new DiscoverNewFriendsAdapter(this, discoverFriendList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(DiscoverFriendsActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(discoverNewFriendsAdapter);
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        /**
+         * Do something when user clicked on the
+         * add friend icon
+         */
+        AppUtils.toast(
+                DiscoverFriendsActivity.this,
+                discoverFriendList.get(position).getName() + " is added as friend"
+        );
+
+        discoverFriendList.remove(position);
+        discoverNewFriendsAdapter.notifyItemRemoved(position);
+
+    }
+
+    @Override
+    public void onItemLongClick(int position) {
+        /**
+         * Ignore this method for now
+         */
+    }
 }
