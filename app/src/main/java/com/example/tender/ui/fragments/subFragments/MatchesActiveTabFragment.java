070cd01de62d192a0467399c7134353724ee2532
kphilemon <kphilemon0529@gmail.com>
2022-02-07 04:58:57 +0800

    Integrate matches tab

--- app/src/main/java/com/example/tender/ui/fragments/subFragments/MatchesActiveTabFragment.java
+++ app/src/main/java/com/example/tender/ui/fragments/subFragments/MatchesActiveTabFragment.java
@@ -1,44 +1,72 @@
 package com.example.tender.ui.fragments.subFragments;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.tender.R;
-import com.example.tender.model.ActiveMatches;
+import com.example.tender.model.Match;
+import com.example.tender.model.MatchWrapper;
 import com.example.tender.model.User;
-import com.example.tender.utils.adapter.recyclerAdapter.MatchesActiveRvAdapter;
+import com.example.tender.ui.RecyclerViewInterface;
+import com.example.tender.ui.activity.MatchEnterNameActivity;
+import com.example.tender.ui.activity.MatchResultsPendingActivity;
+import com.example.tender.ui.activity.SwipeActivity;
+import com.example.tender.utils.adapter.recyclerAdapter.MatchesListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.todkars.shimmer.ShimmerRecyclerView;
 
 import java.util.ArrayList;
 
 
-public class MatchesActiveTabFragment extends Fragment {
-
-    private ArrayList<ActiveMatches> activeMatches;
+public class MatchesActiveTabFragment extends Fragment implements RecyclerViewInterface {
+    private Context context;
+    private ArrayList<MatchWrapper> activeMatches;
+    private MatchesListAdapter matchesListAdapter;
     private ShimmerRecyclerView recyclerView;
+    private TextView emptyView;
+
+    private FirebaseFirestore db;
+    private FirebaseUser firebaseUser;
+    private ListenerRegistration registration;
 
     public MatchesActiveTabFragment() {
         // Required empty public constructor
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         activeMatches = new ArrayList<>();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_matches_active_tab, container, false);
     }
@@ -46,48 +74,98 @@ public class MatchesActiveTabFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.matches_active_tab_recycler_view);
+        matchesListAdapter = new MatchesListAdapter(context, firebaseUser.getUid(), activeMatches, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(matchesListAdapter);
+        emptyView = view.findViewById(R.id.matches_active_tab_empty_view);
+        updateUI(true, false);
+        startPolling();
+    }
 
-        recyclerView = view.findViewById(R.id.active_tab_frag_recyclerView);
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.context = null;
+        stopPolling();
+    }
 
-        setupUserInfo();
-        setAdapter();
-        //recyclerView.showShimmer();
+    @Override
+    public void onItemClick(int position) {
+        MatchWrapper target = activeMatches.get(position);
+
+        if (target.getMatch().getSwipes().containsKey(firebaseUser.getUid())) {
+            Intent intent = new Intent(context, MatchResultsPendingActivity.class);
+            intent.putExtra("MATCH_ID", target.getId());
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(context, SwipeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.putExtra("MATCH_ID", target.getId());
+            intent.putExtra("MATCH_NAME", target.getMatch().getName());
+            intent.putExtra("MATCH_DISPLAY_NAMES", target.getMatch().getDisplayNames());
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onItemLongClick(int position) {
 
     }
 
-    public void setAdapter(){
-        MatchesActiveRvAdapter matchesActiveAdapter = new MatchesActiveRvAdapter(activeMatches);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(matchesActiveAdapter);
+    private void startPolling() {
+        registration = db.collection("users").document(firebaseUser.getUid())
+                .addSnapshotListener((snapshot, e) -> {
+                    activeMatches.clear();
 
+                    if (e != null) {
+                        Log.w("TAG", "Listen failed.", e);
+                        updateUI(false, true);
+                        return;
+                    }
+
+                    if (snapshot == null || !snapshot.exists()) {
+                        updateUI(false, true);
+                        return;
+                    }
+
+                    User user = snapshot.toObject(User.class);
+                    assert user != null;
+
+                    if (user.getActiveMatches() == null || user.getActiveMatches().size() == 0) {
+                        updateUI(false, true);
+                        return;
+                    }
+
+                    db.collection("matches").whereIn(FieldPath.documentId(), user.getActiveMatches())
+                            .get()
+                            .addOnSuccessListener(queryDocumentSnapshots -> {
+                                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                                    Match m = documentSnapshot.toObject(Match.class);
+                                    activeMatches.add(new MatchWrapper(documentSnapshot.getId(), m));
+                                }
+                                updateUI(false, false);
+                            })
+                            .addOnFailureListener(e1 -> {
+                                Log.w("TAG", "Load matches from ids failed.", e1);
+                                updateUI(false, true);
+                            });
+                });
     }
 
-    /**
-     * Dummy data set
-     */
-    private void setupUserInfo(){
-        activeMatches.add(new ActiveMatches(
-                "https://cdn.stocksnap.io/img-thumbs/960w/macro-flower_LBA8TQASNP.jpg",
-                "Match 1",
-                "Awesome lunch",
-                "10.30 a.m.",
-                0
-        ));
-        activeMatches.add(new ActiveMatches(
-                "https://cdn.stocksnap.io/img-thumbs/960w/macro-flower_LBA8TQASNP.jpg",
-                "Match 2",
-                "Awesome Dinner",
-                "7.30 p.m.",
-                1
-        ));
-        activeMatches.add(new ActiveMatches(
-                "https://cdn.stocksnap.io/img-thumbs/960w/macro-flower_LBA8TQASNP.jpg",
-                "Match 3",
-                "Awesome breakfast",
-                "8.30 a.m.",
-                1
-        ));
 
+    private void stopPolling() {
+        if (registration != null) {
+            registration.remove();
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void updateUI(boolean showShimmer, boolean showEmptyView) {
+        matchesListAdapter.notifyDataSetChanged();
+        if (showShimmer) recyclerView.showShimmer();
+        else recyclerView.hideShimmer();
+        emptyView.setVisibility(showEmptyView ? View.VISIBLE : View.GONE);
     }
 }
--- app/src/main/java/com/example/tender/ui/fragments/subFragments/MatchesCompletedTabFragment.java
+++ app/src/main/java/com/example/tender/ui/fragments/subFragments/MatchesCompletedTabFragment.java
@@ -1,32 +1,71 @@
 package com.example.tender.ui.fragments.subFragments;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.tender.R;
+import com.example.tender.model.Match;
+import com.example.tender.model.MatchWrapper;
+import com.example.tender.model.User;
+import com.example.tender.model.UserWrapper;
+import com.example.tender.ui.RecyclerViewInterface;
+import com.example.tender.ui.activity.MatchResultsPendingActivity;
+import com.example.tender.ui.activity.SwipeActivity;
+import com.example.tender.utils.adapter.recyclerAdapter.MatchesListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.todkars.shimmer.ShimmerRecyclerView;
 
-public class MatchesCompletedTabFragment extends Fragment {
+import java.util.ArrayList;
+
+public class MatchesCompletedTabFragment extends Fragment implements RecyclerViewInterface {
+    private Context context;
+    private ArrayList<MatchWrapper> completedMatches;
+    private MatchesListAdapter matchesListAdapter;
+    private ShimmerRecyclerView recyclerView;
+    private TextView emptyView;
+
+    private FirebaseFirestore db;
+    private FirebaseUser firebaseUser;
+    private ListenerRegistration registration;
 
     public MatchesCompletedTabFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        completedMatches = new ArrayList<>();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_matches_completed_tab, container, false);
     }
@@ -34,9 +73,88 @@ public class MatchesCompletedTabFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.matches_completed_tab_recycler_view);
+        matchesListAdapter = new MatchesListAdapter(context, firebaseUser.getUid(), completedMatches, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(matchesListAdapter);
+        emptyView = view.findViewById(R.id.matches_completed_tab_empty_view);
+        updateUI(true, false);
+        startPolling();
+    }
 
-        ShimmerRecyclerView recyclerView = view.findViewById(R.id.matches_completed_tab_recyclerView);
-        recyclerView.showShimmer();
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.context = null;
+        stopPolling();
+    }
 
+    @Override
+    public void onItemClick(int position) {
+        MatchWrapper target = completedMatches.get(position);
+        Intent intent = new Intent(context, MatchResultsPendingActivity.class);
+        intent.putExtra("MATCH_ID", target.getId());
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onItemLongClick(int position) {
+
+    }
+
+    private void startPolling() {
+        registration = db.collection("users").document(firebaseUser.getUid())
+                .addSnapshotListener((snapshot, e) -> {
+                    completedMatches.clear();
+
+                    if (e != null) {
+                        Log.w("TAG", "Listen failed.", e);
+                        updateUI(false, true);
+                        return;
+                    }
+
+                    if (snapshot == null || !snapshot.exists()) {
+                        updateUI(false, true);
+                        return;
+                    }
+
+                    User user = snapshot.toObject(User.class);
+                    assert user != null;
+
+                    if (user.getCompletedMatches() == null || user.getCompletedMatches().size() == 0) {
+                        updateUI(false, true);
+                        return;
+                    }
+
+                    db.collection("matches").whereIn(FieldPath.documentId(), user.getCompletedMatches())
+                            .get()
+                            .addOnSuccessListener(queryDocumentSnapshots -> {
+                                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                                    Match m = documentSnapshot.toObject(Match.class);
+                                    completedMatches.add(new MatchWrapper(documentSnapshot.getId(), m));
+                                }
+                                updateUI(false, false);
+                            })
+                            .addOnFailureListener(e1 -> {
+                                Log.w("TAG", "Load matches from ids failed.", e1);
+                                updateUI(false, true);
+                            });
+                });
+    }
+
+
+    private void stopPolling() {
+        if (registration != null) {
+            registration.remove();
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void updateUI(boolean showShimmer, boolean showEmptyView) {
+        matchesListAdapter.notifyDataSetChanged();
+        if (showShimmer) recyclerView.showShimmer();
+        else recyclerView.hideShimmer();
+        emptyView.setVisibility(showEmptyView ? View.VISIBLE : View.GONE);
     }
 }
